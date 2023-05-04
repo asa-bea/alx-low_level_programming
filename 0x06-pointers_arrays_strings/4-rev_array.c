34beb2fcdf49eae401f480efbde4bdea8e6bb997
asa-bea <akosasab@yahoo.com>
2023-05-04 06:22:32 -0700

    Task 4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * reverse_array - reverse array
+ * @a:array
+ * @n:integer
+ * Return:void
+ */
+void reverse_array(int *a, int n)
+{
+int x, y;
+
+for (x = 0; (x < (n - 1) / 2); x++)
+	{
+	y = a[x];
+	a[x] = a[n - 1 - x];
+	a[n - 1 - x] = y;
+	}
+}
