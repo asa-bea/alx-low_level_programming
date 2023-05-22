ba86707b5159da4babc75d158a889f12794f813a
asa-bea <akosasab@yahoo.com>
2023-05-22 09:39:50 -0700

    task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,14 +2,14 @@
 #include "dog.h"
 
 /**
- * printnew_dog - creates a new dog
+ * new_dog - creates a new dog
  * @name: name of dog
  * @age: age of dog
  * @owner: owner of dog
  *
  * Return: pointer to new dog
  */
-dog_t *printnew_dog(char *name, float age, char *owner)
+dog_t *new_dog(char *name, float age, char *owner)
 {
 	unsigned int nl, ol, i;
 	dog_t *dog;
