283401000d0352a6308ce352e9f64c522b8c7c92
asa-bea <akosasab@yahoo.com>
2023-05-24 13:51:42 -0700

    task 3

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -3,30 +3,27 @@
 
 /**
  * get_op_func - Entry point
+ * Desc - 'function that selects the correct function
+ * to perform the operation asked by the user'
  * @s: the operator passed as argument given by the user
  *
- * Return: a pointer to the func which corresponds to the operator given as
- * a parameter, or NULL if s does not match any of expected operators
+ * Return : --
  */
+
 int (*get_op_func(char *s))(int, int)
 {
+
 	op_t ops[] = {
-	{ "+", op_add },
-	{ "-", op_sub },
-	{ "*", op_mul },
-	{ "/", op_div },
-	{ "%", op_mod },
-	{ NULL, NULL }
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
 	};
-	int i = 0;
+	int h = 0;
 
-	while (i < 5)
-	{
-		if (strcmp(s, ops[i].op) == 0)
-			return (ops[i].f);
-
-		i++;
-	}
-
-	return (0);
+	while (ops[h].op != NULL && *(ops[h].op) != *s)
+		h++;
+	return (ops[h].f);
 }
